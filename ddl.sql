145dee0f501e6727f95e683002107417db275fb4
Vincent <1061271330@qq.com>
2019-07-03 02:42:47 +0800

    Vincent's commit

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,48 @@
--- **数据库级别：**  
+﻿-- **数据库级别：**  
 --  显示所有数据库  
+show DATABASES;
 
 --  进入某个数据库  
+use student_examination_sys;
 
 --  创建一个数据库  
+create table student_examination_sys;
 
---  创建指定字符集的数据库  
+--  创建指定字符集的数据库
+GBK: create database student_examination_sys DEFAULT CHARACTER SET gbk COLLATE gbk_chinese_ci;
+UTF8: CREATE DATABASE student_examination_sys DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci；
 
 --  显示数据库的创建信息   
+show create database student_examination_sys;
 
 --  修改数据库的编码  
+ALTER DATABASE student_examination_sys CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
---  删除一个数据库   
+--  删除一个数据库
+drop database student_examination_sys;
+   
 
 -- **表级别**
 --  修改表名
+rename table students to student;
 
 --  修改字段的数据类型
+alter table student modify age varchar(10);
 
 --  修改字段名
+alter table student change name stu_name varchar(10);
 
 --  添加字段
+alter table student add column newcolumn  varchar(10);
 
 --  删除字段
+alter table student drop newcolumn;
 
 --  修改表的存储引擎
+alter table student ENGINE=InnoDB
 
 --  删除表的外键约束
+alter table score drop FOREIGN key student_id;
 
 --  删除一张表
+drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
--- 插入记录  
+﻿-- 插入记录
+insert into student values ('001','张三',18,'男');
 
--- 修改记录  
+-- 修改记录
+update students set name='张公子' where id='001';
 
---  删除记录  
+--  删除记录
+delete from students where id='001';  
 
--- 查询记录  
+-- 查询记录 
+SELECT * from students; 
